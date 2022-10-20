c8aa977398cef0a5b56b215a3778277e179b3f83
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2022-10-20 09:21:24 -0700

    codewars

---
+++ String-repeat/main.js
@@ -0,0 +1,18 @@
+// Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+// Examples (input -> output)
+// 6, "I"     -> "IIIIII"
+// 5, "Hello" -> "HelloHelloHelloHelloHello"
+
+
+//My solution
+
+const repeatStr = (n, s) => s.repeat(n)
+
+
+//Best solution
+//My thoughts: Similar 
+
+function repeatStr (n, s) {
+    return s.repeat(n);
+  }
