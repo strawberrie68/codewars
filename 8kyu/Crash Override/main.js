c4abc55368566b7e669429a83ea92a5000b8a6e9
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2022-12-18 06:14:00 -0800

    codewars

---
+++ 8kyu/Crash Override/main.js
@@ -0,0 +1,40 @@
+// Every budding hacker needs an alias! The Phantom Phreak, Acid Burn, Zero Cool and Crash Override are some notable examples from the film Hackers.
+
+// Your task is to create a function that, given a proper first and last name, will return the correct alias.
+
+// Notes:
+// Two objects that return a one word name in response to the first letter of the first name and one for the first letter of the surname are already given.
+
+// If the first character of either of the names given to the function is not a letter from A - Z, you should return "Your name must start with a letter from A - Z."
+
+// Sometimes people might forget to capitalize the first letter of their name so your function should accommodate for these grammatical errors.
+
+// Examples
+// var firstName = {A: 'Alpha', B: 'Beta', C: 'Cache' ...}
+// var surname = {A: 'Analogue', B: 'Bomb', C: 'Catalyst' ...}
+
+// aliasGen('Larry', 'Brentwood') === 'Logic Bomb'
+// aliasGen('123abc', 'Petrovic') === 'Your name must start with a letter from A - Z.'
+// Happy hacking!
+
+//My solution
+
+function aliasGen(first, last){
+    let firstChar = first.toUpperCase().charAt(0)
+    let lastChar = last.toUpperCase().charAt(0)
+      if(/[0-9]/.test(firstChar) || /[0-9]/.test(lastChar)){
+        return "Your name must start with a letter from A - Z."
+      }else{
+        return firstName[firstChar] +" "+ surname[lastChar] 
+      }
+  }
+
+//Better to write like this though
+
+const initialCap = (str) => str[0].toUpperCase();
+const isValidName = (name) => /^[a-z]/i.test(name);
+const aliasGen = (fName, lName) => {
+  return (isValidName(fName) && isValidName(lName))
+    ? `${ firstName[initialCap(fName)] } ${ surname[initialCap(lName)] }`
+    : 'Your name must start with a letter from A - Z.';
+}
