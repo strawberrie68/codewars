28fb01212c5d9297ed640a3999b5cc38daa9134c
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2023-03-01 20:24:26 -0800

    codewwars

--- 8kyu/Drink about/main.js
+++ 8kyu/Drink about/main.js
@@ -24,3 +24,9 @@
 function peopleWithAgeDrink(old) {
     return `drink ${old <14 ? `toddy`: old<18 ?`coke`: old<21? `beer`: `whisky`}`
   };
+
+  //Other Solution
+  const peopleWithAgeDrink = (age) =>
+  age < 14 ? "drink toddy" :
+  age < 18 ? "drink coke" :
+  age < 21 ? "drink beer" : "drink whisky"
