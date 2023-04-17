3a2a05a96ee772b89fbfb2f78a2b962011c37966
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2023-04-17 15:05:31 -0700

    codewars

---
+++ 7kyu/No oddities here/main.js
@@ -0,0 +1,16 @@
+// Write a small function that returns the values of an array that are not odd.
+
+// All values in the array will be integers. Return the good values in the order they are given.
+
+
+//My solution
+
+function noOdds( values ){
+    let odd = []
+    values.map((a,i)=> a % 2 == 0 ? odd.push(a) : '' )
+    return odd
+
+}
+
+//Other Solutions
+var noOdds = values => values.filter(x => x % 2 === 0);
