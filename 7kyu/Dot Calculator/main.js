e479223992769527b10b7ffc86aec59ffc201eea
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2023-04-30 08:01:00 -0700

    codewars

---
+++ 7kyu/Dot Calculator/main.js
@@ -0,0 +1,46 @@
+// You have to write a calculator that receives strings for input. The dots will represent the number in the equation. There will be dots on one side, an operator, and dots again after the operator. The dots and the operator will be separated by one space.
+
+// Here are the following valid operators :
+
+// + Addition
+// - Subtraction
+// * Multiplication
+// // Integer Division
+// Your Work (Task)
+// You'll have to return a string that contains dots, as many the equation returns. If the result is 0, return the empty string. When it comes to subtraction, the first number will always be greater than or equal to the second number.
+
+// Examples (Input => Output)
+// * "..... + ..............." => "...................."
+// * "..... - ..."             => ".."
+// * "..... - ."               => "...."
+// * "..... * ..."             => "..............."
+// * "..... * .."              => ".........."
+// * "..... // .."             => ".."
+// * "..... // ."              => "....."
+// * ". // .."                 => ""
+// * ".. - .."                 => ""
+
+//My solution
+
+function dotCalculator (equation) {
+	let number = equation.split(' ')
+  let dots = ''
+  let dotNumber =  Math.floor(eval(number[0].length + (number[1] === '//'? '/' : number[1]) + number[2].length))
+  for(let i = 0; i < dotNumber; i++){
+    dots += '.'
+  }
+  return dots
+}
+
+
+//Other possible ways
+const dotCalculator = (equation) => {
+    const operations = {
+      '+' : (a, b) => a + b,
+      '-' : (a, b) => a - b,
+      '*' : (a, b) => a * b,
+      '//': (a, b) => a / b,
+    };
+    const [left, operator, right] = equation.split(' ');
+    return '.'.repeat(operations[operator](left.length, right.length));
+  }
