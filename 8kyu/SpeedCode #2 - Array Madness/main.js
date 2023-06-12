f7831291a1eb0cd2bbad29860c3df7f6e620de94
Michelle Wong <42231000+strawberrie68@users.noreply.github.com>
2023-06-11 18:29:40 -0700

    codewars

---
+++ 8kyu/SpeedCode #2 - Array Madness/main.js
@@ -0,0 +1,29 @@
+// SpeedCode #2 - Array Madness
+// Objective
+// Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b.
+
+// E.g.
+
+// arrayMadness([4, 5, 6], [1, 2, 3]); // returns true since 4 ** 2 + 5 ** 2 + 6 ** 2 > 1 ** 3 + 2 ** 3 + 3 ** 3
+// Get your timer out. Are you ready? Ready, get set, GO!!!
+
+//solution
+
+
+const arrayMadness = (a, b) => a.reduce((acc, x) => acc + x**2, 0) > b.reduce((acc, x) => acc + x**3, 0) 
+
+
+//or
+
+function arrayMadness(a, b) {
+    return a.reduce( (sum, el) => sum + el ** 2, 0) >
+           b.reduce( (sum, el) => sum + el ** 3, 0);
+  }
+
+  //or
+
+  function arrayMadness(a, b) {
+    let firstArray = a.map((numA) => Math.pow(numA,2)).reduce((acc,c)=>acc+c) 
+    let secondArray = b.map((numB) => Math.pow(numB,3)).reduce((acc,c)=>acc+c) 
+    return firstArray > secondArray
+  }
